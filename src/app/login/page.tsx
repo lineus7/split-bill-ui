81e67e3b8a59bc5c1af3067e7cd81b5b3c4ea0f8
Ryan Willyanto <ryanwillyanto@gmail.com>
2025-08-19 16:42:14 +0700

    Add image-placeholder

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,11 +3,14 @@ import { BasePasswordInput } from "../../components/BasePasswordInput";
 import { BaseTextField } from "../../components/BaseTextField";
 import { BaseButton } from "../../components/BaseButton";
 import { BaseText } from "../../components/BaseText";
+import Image from "next/image";
 
 export default function LoginPage() {
   return (
     <div className="h-screen">
-      <div className="h-[40%] bg-highlight-300" />
+      <Container className="h-[40%] bg-highlight-300 flex items-center justify-center">
+        <Image src="/image-placeholder.svg" alt="" width={33} height={32} />
+      </Container>
 
       <Container className="h-[60%] bg-light-300 flex flex-col">
         <div className="flex-1 py-10 flex flex-col justify-between">
