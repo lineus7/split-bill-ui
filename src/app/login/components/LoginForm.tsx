857a45fe09792ea74417f57f97a8af4faa023a68
Ryan Willyanto <ryanwillyanto@gmail.com>
2025-08-19 23:29:43 +0700

    Add Login Server Action

--- src/app/login/components/LoginForm.tsx
+++ src/app/login/components/LoginForm.tsx
@@ -4,23 +4,41 @@ import { BaseButton } from "@/components/BaseButton";
 import { BasePasswordInput } from "@/components/BasePasswordInput";
 import { BaseText } from "@/components/BaseText";
 import { BaseTextField } from "@/components/BaseTextField";
+import { useActionState } from "react";
+import { loginUser } from "../actions/action";
 
 export default function LoginForm() {
+    const [state, formAction, pending] = useActionState(loginUser, {
+        errors: undefined,
+        message: "",
+    });
     return (
-        <div className="flex-1 py-10 flex flex-col justify-between">
+        <form
+            action={formAction}
+            className="flex-1 py-10 flex flex-col justify-between"
+        >
             <div className="flex flex-col gap-4">
                 <BaseText variant="title">Welcome</BaseText>
 
                 <div className="flex flex-col gap-4">
-                    <BaseTextField label="Email" autoComplete="email" />
+                    <BaseTextField
+                        label="Email"
+                        autoComplete="email"
+                        name="email"
+                        error={!!state.errors?.email}
+                        helperText={state.errors?.email?.[0]}
+                    />
                     <BasePasswordInput
                         label="Password"
                         autoComplete="current-password"
+                        name="password"
+                        error={!!state.errors?.password}
+                        helperText={state.errors?.password?.[0]}
                     />
                 </div>
             </div>
 
-            <BaseButton label="Login" />
-        </div>
+            <BaseButton type="submit" label="Login" loading={pending} />
+        </form>
     );
 }
--- src/components/BasePasswordInput.tsx
+++ src/components/BasePasswordInput.tsx
@@ -1,46 +1,55 @@
 "use client";
 
 import {
-  FormControl,
-  IconButton,
-  InputAdornment,
-  InputLabel,
-  OutlinedInput,
-  OutlinedInputProps,
+    FormControl,
+    FormHelperText,
+    IconButton,
+    InputAdornment,
+    InputLabel,
+    OutlinedInput,
+    OutlinedInputProps,
 } from "@mui/material";
 import Visibility from "@mui/icons-material/Visibility";
 import VisibilityOff from "@mui/icons-material/VisibilityOff";
 import { useState } from "react";
 
+interface BasePasswordInputProps extends OutlinedInputProps {
+    helperText?: string;
+}
+
 export const BasePasswordInput = ({
-  size = "medium",
-  ...props
-}: OutlinedInputProps) => {
-  const [showPassword, setShowPassword] = useState(false);
-  const handleClickShowPassword = () => setShowPassword((show) => !show);
-  return (
-    <FormControl variant="outlined" size={size}>
-      <InputLabel htmlFor="outlined-adornment-password">
-        {props.label}
-      </InputLabel>
-      <OutlinedInput
-        id="outlined-adornment-password"
-        type={showPassword ? "text" : "password"}
-        endAdornment={
-          <InputAdornment position="end">
-            <IconButton
-              aria-label={
-                showPassword ? "hide the password" : "display the password"
-              }
-              onClick={handleClickShowPassword}
-              edge="end"
-            >
-              {showPassword ? <VisibilityOff /> : <Visibility />}
-            </IconButton>
-          </InputAdornment>
-        }
-        {...props}
-      />
-    </FormControl>
-  );
+    size = "medium",
+    helperText,
+    ...props
+}: BasePasswordInputProps) => {
+    const [showPassword, setShowPassword] = useState(false);
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+    return (
+        <FormControl variant="outlined" size={size} error={props.error}>
+            <InputLabel htmlFor="outlined-adornment-password">
+                {props.label}
+            </InputLabel>
+            <OutlinedInput
+                id="outlined-adornment-password"
+                type={showPassword ? "text" : "password"}
+                endAdornment={
+                    <InputAdornment position="end">
+                        <IconButton
+                            aria-label={
+                                showPassword
+                                    ? "hide the password"
+                                    : "display the password"
+                            }
+                            onClick={handleClickShowPassword}
+                            edge="end"
+                        >
+                            {showPassword ? <VisibilityOff /> : <Visibility />}
+                        </IconButton>
+                    </InputAdornment>
+                }
+                {...props}
+            />
+            {helperText && <FormHelperText>{helperText}</FormHelperText>}
+        </FormControl>
+    );
 };
