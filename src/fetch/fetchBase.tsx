3c2c3abfb065b34a979eeee6ceadd497eb28e489
Ryan Willyanto <ryanwillyanto@gmail.com>
2025-08-20 15:30:26 +0700

    Add Include Credentials

--- src/fetch/fetchBase.tsx
+++ src/fetch/fetchBase.tsx
@@ -3,7 +3,14 @@ export const fetchBase = async (
     init?: RequestInit
 ) => {
     const base = process.env.NEXT_PUBLIC_BASE_URL;
-    const response = await fetch(`${base}${input}`, init);
+    const response = await fetch(`${base}${input}`, {
+        ...init,
+        headers: {
+            ...init?.headers,
+            "Content-Type": "application/json",
+        },
+        credentials: "include",
+    });
     if (!response.ok) {
         const error = await response.json();
         throw error;
