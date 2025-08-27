50e7d40387f746ff80a49d84284266c6f98fd686
Ryan Willyanto <ryanwillyanto@gmail.com>
2025-08-28 06:40:03 +0700

    Add Dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,20 @@
+import { BaseText } from "@/components/BaseText";
+import SearchIcon from "@mui/icons-material/Search";
+
 export default function DashboardPage() {
     return (
-        <div className="h-screen bg-amber-200 flex flex-col">
-            <header className="h-[56px] bg-highlight-300">Header</header>
-            <main className="flex-1 bg-light-300">Main</main>
-            <footer className="h-[72px] bg-dark-300">Footer</footer>
+        <div className="h-screen flex flex-col">
+            <header className="bg-lime-100 relative h-[56px] flex justify-center items-center">
+                <BaseText variant="heading">Transactions</BaseText>
+                <div className="absolute right-2 h-full flex items-center">
+                    <SearchIcon
+                        className="text-highlight-700"
+                        fontSize="large"
+                    />
+                </div>
+            </header>
+            <main className="flex-1 bg-light-300"></main>
+            <footer className="h-[72px] bg-dark-300"></footer>
         </div>
     );
 }
--- src/components/BaseText.tsx
+++ src/components/BaseText.tsx
@@ -1,25 +1,28 @@
 import { cn } from "@/utils/style";
 
 type BaseTextProps = {
-  children: React.ReactNode;
-  variant?: "title" | "subtitle" | "body";
+    children: React.ReactNode;
+    variant?: "title" | "subtitle" | "heading" | "body";
 };
 
 export const BaseText = ({ children, variant = "body" }: BaseTextProps) => {
-  let baseClassName = "";
-  switch (variant) {
-    case "title":
-      baseClassName = "text-2xl font-extrabold";
-      break;
-    case "subtitle":
-      baseClassName = "text-xl font-bold";
-      break;
-    case "body":
-      baseClassName = "text-base";
-      break;
-    default:
-      baseClassName = "text-base";
-      break;
-  }
-  return <p className={cn(baseClassName)}>{children}</p>;
+    let baseClassName = "";
+    switch (variant) {
+        case "title":
+            baseClassName = "text-2xl font-extrabold";
+            break;
+        case "subtitle":
+            baseClassName = "text-xl font-bold";
+            break;
+        case "heading":
+            baseClassName = "text-[14px] font-bold";
+            break;
+        case "body":
+            baseClassName = "text-base";
+            break;
+        default:
+            baseClassName = "text-base";
+            break;
+    }
+    return <p className={cn(baseClassName)}>{children}</p>;
 };
