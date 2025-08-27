a84a413a527dee0249e24011d868c5f51a4f5522
Ryan Willyanto <ryanwillyanto@gmail.com>
2025-08-27 14:01:03 +0700

    fix Error Notifier

--- src/hooks/useErrorNotifier.tsx
+++ src/hooks/useErrorNotifier.tsx
@@ -2,11 +2,15 @@ import { useEffect } from "react";
 import { toast } from "@/utils/toast";
 
 export const useErrorNotifier = (error: any) => {
-    useEffect(() => {
-        console.log(error);
+  useEffect(() => {
+    console.log(error);
 
-        if (error?.Message) {
-            toast.error(error.Message);
-        }
-    }, [error]);
+    if (error) {
+      if (error?.Message) {
+        toast.error(error.Message);
+      } else {
+        toast.error("Something went wrong");
+      }
+    }
+  }, [error]);
 };
