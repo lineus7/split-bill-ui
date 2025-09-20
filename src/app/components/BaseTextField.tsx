51637db89093159856becaf7c19b4d2871ba60ec
Ryan Willyanto <ryanwillyanto@gmail.com>
2025-09-20 14:07:47 +0700

    Remove Unused Component

--- src/app/components/BaseTextField.tsx
+++
@@ -1,8 +0,0 @@
-import { TextField, TextFieldProps } from "@mui/material";
-
-export const BaseTextField = ({
-  size = "medium",
-  ...props
-}: TextFieldProps) => {
-  return <TextField size={size} {...props} />;
-};
