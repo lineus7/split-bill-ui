82bfda0d1ee90eda05ed8c695ed2bfc501f18fd2
Ryan Willyanto <ryanwillyanto@gmail.com>
2025-10-17 10:19:12 +0700

    Fix Profile Service

--- src/app/(auth)/profile/services/profileService.tsx
+++ src/app/(auth)/profile/services/profileService.tsx
@@ -3,8 +3,8 @@ import { BaseResponse } from "@/types/response";
 
 export const profileService = {
     changePassword: async (currentPassword: string, newPassword: string) => {
-        return api.post<BaseResponse<null>>("/profile/change-password", {
-            old_password: currentPassword,
+        return api.post<BaseResponse<null>>("/api/profile/change-password", {
+            current_password: currentPassword,
             new_password: newPassword,
         });
     },
